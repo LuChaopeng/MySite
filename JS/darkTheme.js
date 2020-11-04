fe3458267b8154a545aba5aa50f0032e9b29be44
MingLinux <chaopeng.lu@qq.com>
2020-11-05 03:10:45 +0800

    修复和优化

--- JS/darkTheme.js
+++ JS/darkTheme.js
@@ -13,7 +13,7 @@ function lightTheme(){
     document.documentElement.style.setProperty("--headerBGC","rgb(172, 176, 209)");
     document.documentElement.style.setProperty("--aColor","black");
     document.documentElement.style.setProperty("--aHoverBGC","rgb(139, 145, 204)");
-    document.getElementsByClassName("dark")[0].getElementsByTagName("span")[0].firstChild.nodeValue="Light Mode";
+    document.getElementsByClassName("dark")[0].getElementsByTagName("span")[0].firstChild.nodeValue="Dark Mode";
 }
 let theme = document.getElementsByClassName("dark")[0];
 
